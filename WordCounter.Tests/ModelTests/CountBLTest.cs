a14923104c94f720f8aab2e488b47e708ff8b4a7
charley mcgowan <charley.mcgowan@outlook.com>
2018-11-16 08:29:55 -0800

    everything has been set up to start the project

---
+++ WordCounter.Tests/ModelTests/CountBLTest.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace WordCounter.Tests
+{
+  [TestClass]
+  public class CountBLTest
+  {
+    [TestMethod]
+        public void CHANGEME()
+        {
+          CountBL testCountBL = new CountBL();
+          Assert.AreEqual(false, testCountBL.CHANGEME());
+        }
+  }
+}
